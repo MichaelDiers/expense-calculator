f81fd2ed39e7171928808cf9a1d08ffece2c5426
MichaelDiers <md@sreid.de>
2025-04-08 23:38:20 +0200

    add projects

---
+++ ExpenseCalculator/ExpenseCalculator.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace ExpenseCalculator.Wpf;
+
+using System.Windows;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+}
